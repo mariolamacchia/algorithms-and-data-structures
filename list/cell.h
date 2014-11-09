be50c0786feb36cb9e15810fcf1366a9faa1260b
Mario Lamacchia <mariolamacchia@gmail.com>
2014-11-09 21:30:36 +0100

    Implement list

--- list/cell.h
+++ list/cell.h
@@ -4,6 +4,24 @@
 template <class T>
 class Cell 
 {
+    public:
+        Cell() {next = NULL; previous = NULL;}
+        ~Cell() {}
+
+        T getValue() {return value;}
+        void setValue(T v) {value = v;}
+
+        Cell* getNext() {return next;}
+        void setNext(Cell* c) {next = c;}
+
+        Cell* getPrevious() {return previous;}
+        void setPrevious(Cell* c) {previous = c;}
+
+    private:
+        T value;
+        Cell<T>* next;
+        Cell<T>* previous;
+        static const T EMPTY_VALUE;
 };
 
 #endif
--- list/list.h
+++ list/list.h
@@ -25,87 +25,118 @@ class List
 
         void insert(Cell<T>*, T);
         void remove(Cell<T>*);
+    private:
+        Cell<T>* pivot;
 };
 
 template <class T>
 List<T>::List()
 {
+    createList();
 }
 
 template <class T>
-List<T>::List(const List&)
+List<T>::List(const List & l)
 {
+    Cell<T>* c1, *c2;
+    createList();
+    c1 = l.getFirst();
+    c2 = getFirst();
+    while (!l.eol(c1))
+    {
+        insert(c2, l.read(c1));
+        c1 = l.getNext(c1);
+    }
 }
 
 template <class T>
 List<T>::~List()
 {
+    Cell<T>* c;
+    while (!isEmpty())
+    {
+        remove(getFirst());
+    }
+    delete pivot;
 }
 
 
 template <class T>
 void List<T>::createList()
 {
+    pivot = new Cell<T>;
+    pivot->setNext(pivot);
+    pivot->setPrevious(pivot);
 }
 
 
 template <class T>
-bool List<T>::eol(Cell<T>*) const
+bool List<T>::eol(Cell<T>* c) const
 {
-    return true;
+    return c->getValue() == pivot->getValue();
 }
 
 
 template <class T>
 bool List<T>::isEmpty() const
 {
-    return true;
+    return eol(getFirst());
 }
 
 
 template <class T>
 Cell<T>* List<T>::getFirst() const
 {
-    return NULL;
+    return pivot->getNext();
 }
 
 
 template <class T>
-Cell<T>* List<T>::getNext(Cell<T>*) const
+Cell<T>* List<T>::getNext(Cell<T>* c) const
 {
-    return NULL;
+    return c->getNext();
 }
 
 
 template <class T>
-Cell<T>* List<T>::getPrevious(Cell<T>*) const
+Cell<T>* List<T>::getPrevious(Cell<T>* c) const
 {
-    return NULL;
+    return c->getPrevious();
 }
 
 
 template <class T>
-T List<T>::read(Cell<T>*) const
+T List<T>::read(Cell<T>* c) const
 {
-    return 0;
+    return c->getValue();
 }
 
 
 template <class T>
-void List<T>::write(Cell<T>*, T)
+void List<T>::write(Cell<T>* c, T v)
 {
+    c->setValue(v);
 }
 
 
 template <class T>
-void List<T>::insert(Cell<T>*, T)
+void List<T>::insert(Cell<T>* c, T v)
 {
+    Cell<T> *n = new Cell<T>;
+    n->setValue(v);
+    n->setNext(c);
+    n->setPrevious(c->getPrevious());
+    c->getPrevious()->setNext(n);
+    c->setPrevious(n);
 }
 
 
 template <class T>
-void List<T>::remove(Cell<T>*)
+void List<T>::remove(Cell<T>* c)
 {
+    c->getPrevious()->setNext(c->getNext());
+    c->getNext()->setPrevious(c->getPrevious());
+    delete c;
 }
 
 

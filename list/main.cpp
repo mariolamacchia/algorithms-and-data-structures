52624ef6e434233ce2703948a48142d8cf495cf6
Mario Lamacchia <mariolamacchia@gmail.com>
2015-01-10 17:49:51 +0100

    Create tests

--- list/main.cpp
+++ list/main.cpp
@@ -6,16 +6,26 @@
 
 using namespace std;
 
-void printIntList(List<int>* l)
+void printIntList(List<int> *l)
 {
-    cell c = l->getFirst();
-    cout << "["
-    while (!l->isEol(c))
+    typename List<int>::cell c = l->getFirst();
+    cout << "\033[1;31m[";
+    while (!l->eol(c))
     {
         cout << l->read(c) << ", ";
         c = l->getNext(c);
     }
-    cout << "]";
+    cout << "]\033[0m";
+}
+
+void startTest(string it)
+{
+    cout << "\n(it should be \"" << it << "\"): \033[1;31m";
+}
+
+void endTest()
+{
+    cout << "\033[0m\n\n";
 }
 
 int main()
@@ -24,44 +34,45 @@ int main()
     typename List<int>::cell c;
 
     // Test constructor
-    cout << "Create empty list\n";
+    cout << "Create empty list";
     List<int> l1;
 
     // Test isEmpty
-    cout << "Is empty? " << l1.isEmpty() << "\n";
+    cout << "\nIs list empty?";
+    startTest("1");
+    cout << l1.isEmpty();
+    endTest();
 
     // Test insert
-    cout << "Fill with [1, 2, 3, 4, 5]\n";
-
+    cout << "\nFill with [1, 2, 3, 4, 5]";
+    startTest("[1, 2, 3, 4, 5,]");
     c = l1.getFirst();
     for (int i = 0; i < 5; i++) l1.insert(c, i+1);
+    printIntList(&l1);
+    endTest();
 
     // Test getNext and read
-    cout << "Get second element: ";
-
+    cout << "\nGet second element:";
+    startTest("2");
     c = l1.getNext(l1.getFirst());
-
-    cout << l1.read(c) << "\n";
+    cout << l1.read(c);
+    endTest();
 
     // Test getPrevious and write
-    cout << "Get first element and replace with 10: ";
-
+    cout << "\nGet first element and replace with 10:";
     c = l1.getPrevious(c);
-    cout << l1.read(c) << "  > ";
+    startTest("1 > 10");
+    cout << l1.read(c) << " > ";
     l1.write(c, 10);
-    cout << l1.read(l1.getFirst()) << "\n";
+    cout << l1.read(l1.getFirst());
+    endTest();
     
     // Test copy constructor and eol
-    cout << "Create copy of list\n";
+    cout << "Create copy of list";
+    startTest("[10, 2, 3, 4, 5,]");
     List<int> l2(l1);
-    c = l2.getFirst();
-    cout << "[";
-    while (!l2.eol(c))
-    {
-        cout << l2.read(c) << ", ";
-        c = l2.getNext(c);
-    }
-    cout << "]\n";
+    printIntList(&l2);
+    endTest();
 
     // Test remove
     cout << "Empty first list\n";
@@ -69,56 +80,31 @@ int main()
     {
         l1.remove(l1.getFirst());
     }
-
-    cout << "Is copied list empty? " << l2.isEmpty() << "\n";
+    cout << "Is copied list empty?";
+    startTest("1");
+    cout << l2.isEmpty();
+    endTest();
 
     // Merge sort
     List<int>* l;
     l = new List<int>;
     c = l->getFirst();
     for (int i = 0; i < 10; i++) l->insert(c, i);
-    cout << "Merge [1..10]: ";
+    cout << "Merge ordered list [1..10]:";
+    startTest("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]");
     l = mergeSort(l);
-    c = l->getFirst();
-    while (!l->eol(c))
-    {
-        cout << l->read(c) << ", ";
-        c = l->getNext(c);
-    }
-    cout << "\n";
+    printIntList(l);
+    endTest();
 
-    l = new List<int>;
-    c = l->getFirst();
-    for (int i = 10; i > 0; i--) l->insert(c, i);
-    cout << "Merge [1..10]: ";
-    l = mergeSort(l);
-    c = l->getFirst();
-    while (!l->eol(c))
-    {
-        cout << l->read(c) << ", ";
-        c = l->getNext(c);
-    }
-    cout << "\n";
-
+    cout << "Merge random list ";
     l = new List<int>;
     c = l->getFirst();
     for (int i = 10; i > 0; i--) l->insert(c, rand() % 10);
-    cout << "Merge [";
-    c = l->getFirst();
-    while (!l->eol(c))
-    {
-        cout << l->read(c) << ", ";
-        c = l->getNext(c);
-    }
-    cout << "]: ";
+    printIntList(l);
+    startTest("ordered");
     l = mergeSort(l);
-    c = l->getFirst();
-    while (!l->eol(c))
-    {
-        cout << l->read(c) << ", ";
-        c = l->getNext(c);
-    }
-    cout << "\n";
+    printIntList(l);
+    endTest();
 
     cout << "End";
     return 0;
